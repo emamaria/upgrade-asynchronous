2f6d9612ac71f4802fe95118b8390ee1ff3420bd
emamaria <emascript@gmail.com>
2022-03-07 16:06:30 +0100

    chore: add iteracion1.js

--- iteracion1.js
+++ iteracion1.js
@@ -1,7 +1,7 @@
 
-const peticion = fetch('https://api.agify.io?name=michael')
+const peticion = fetch('https://api.agify.io?name=michael');
 
 
 peticion.then(resp => {
       resp.json().then(console.log)
-})
+}).catch(console.log)
